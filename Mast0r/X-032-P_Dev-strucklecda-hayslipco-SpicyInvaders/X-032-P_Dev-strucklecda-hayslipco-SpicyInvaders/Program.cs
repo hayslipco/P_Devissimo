ba859741d1e83a5cf57264e327987b6852ecdcd0
Corwin <hayslipco@etml.educanet2.ch>
2019-05-08 14:22:25 +0200

    Ajout du projet final

---
+++ Mast0r/X-032-P_Dev-strucklecda-hayslipco-SpicyInvaders/X-032-P_Dev-strucklecda-hayslipco-SpicyInvaders/Program.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * ETML
+ * Auteurs: Davor S. et Corwin H.
+ * Date de création: 23.01.19
+ * Description: Classe d'entrée du programme duquel va être lancé le menu puis le jeu
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace X_032_P_Dev_strucklecda_hayslipco_SpicyInvaders
+{
+    /// <summary>
+    /// Classe d'entrée du programme
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Sound.OpenSounds();
+
+            int WINDOW_WIDTH = 140;
+            int WINDOW_HEIGHT = 52;
+
+            Console.CursorVisible = false;
+            Console.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+            //création menu paramètre
+            List<string> parametersOption = new List<string> { "Son", "Affichage", "Difficulté", "Quitter" };
+            List<string> onOffOptionVisual = new List<string> { "Activé / Désactivé", "Activé / Désactivé", "Very Chill / Chill / Not Chill / Chill Norris" };
+
+            Menu parameters = new Menu(">>", ConsoleColor.Yellow, ConsoleColor.DarkMagenta, parametersOption, onOffOptionVisual);
+
+            //création du menu principal
+            List<string> mainMenuOptions = new List<string> { "Jouer", "Paramètres", "Highscores", "Mode d'emploi", "A propos", "Exit" };
+            Menu mainMenu = new Menu(">>", ConsoleColor.Cyan, ConsoleColor.Red, mainMenuOptions, onOffOptionVisual);
+
+            Game game = new Game();
+
+            Sound.MenuTheme(true);
+
+            do
+            {
+                
+                //menuTheme.Play();
+
+                Console.Clear();
+                parameters.DrawTitle();
+                Debug.WriteLine("Début de boucle");
+                mainMenu.EnterMenu();
+
+                //Switch case du menu principal
+                switch (mainMenu.SelectedOption)
+                {
+
+                    case 0:
+                        //menuTheme.Stop();
+                        game.Launch();
+                        Sound.MenuTheme(true);
+                        break;
+
+                    case 1:
+                        // efface l'affichage du menu principal
+                        Console.Clear();
+                        parameters.DrawTitle();
+                        parameters.DrawParameter();
+                        // entre dans les paramètres son, difficulté et affichage                        
+                        do
+                        {
+                            // Affiche les paramètres
+                            parameters.OnOffOptionsVisual();
+                            parameters.Parameters();
+
+
+                            // désactive le son du jeu
+                            if (parameters.SelectedOption == 0)
+                            {
+                                parameters.SoundOption();
+                            }
+                            // changement de l'affichage in-game
+                            if (parameters.SelectedOption == 1)
+                            {
+                                parameters.GameDisplay();
+                            }
+                            if (parameters.SelectedOption == 2)
+                            {
+
+                                Debug.WriteLine("Changement de la difficulté");
+                            }
+                            // efface les options des paramètres                       
+                            if (parameters.SelectedOption == 3)
+                            {
+                                Console.Clear();
+                            }
+                        } while (parameters.SelectedOption != 3);
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Bienvenu dans le highscore");
+                        break;
+
+                    case 3:
+                        ConsoleKeyInfo pressedKey;
+                        mainMenu.ModeDemploi();
+                        do
+                        {
+
+                            pressedKey = Console.ReadKey(true);
+
+                        } while (pressedKey.Key != ConsoleKey.Escape);
+                        Console.Clear();
+
+                        break;
+
+                    case 4:
+                        ConsoleKeyInfo pressed;
+                        mainMenu.Apropos();
+
+                        do
+                        {
+
+                            pressed = Console.ReadKey(true);
+
+                        } while (pressed.Key != ConsoleKey.Escape);
+
+                        Console.Clear();
+
+
+                        break;
+
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+
+                }
+                Debug.WriteLine("fin de boucle");
+            } while (mainMenu.SelectedOption != 5);
+            Debug.WriteLine("Sorti du switch");
+        }
+    }
+}
---
+++ Mast0r/X-032-P_Dev-strucklecda-hayslipco-SpicyInvaders/X-032-P_Dev-strucklecda-hayslipco-SpicyInvaders/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("X-032-P_Dev-strucklecda-hayslipco-SpicyInvaders")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ETML")]
+[assembly: AssemblyProduct("X-032-P_Dev-strucklecda-hayslipco-SpicyInvaders")]
+[assembly: AssemblyCopyright("Copyright © ETML 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("6f0e1507-d96a-4d19-9615-cd83dfa9ac2b")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
